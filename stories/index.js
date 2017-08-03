0120a3b323261e0fe961588941af219a4da44443
Geoff Davis <geoffdavis92@gmail.com>
2017-08-03 02:27:00 -0500

    prettier stories index

--- stories/index.js
+++ stories/index.js
@@ -6,11 +6,16 @@ import { linkTo } from "@storybook/addon-links";
 
 import { Welcome } from "@storybook/react/demo";
 
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-storiesOf("Welcome", module).add("to Storybook", () =>
-	<Welcome showApp={linkTo("Button")} />
-);
+storiesOf("Welcome", module).add("to Storybook", () => <Welcome showApp={linkTo("Button")} />);
+
+class _RenderChildrenFromFn extends Component {
+	render() {
+		const { children, ...rest } = this.props;
+		return children({ ...rest });
+	}
+}
 
 const MenuText = ["Home", "About", "Subjects"];
 
@@ -18,31 +23,6 @@ const colors = {
 	gray: "#ddd",
 	lightgray: "#e5e5e5"
 };
-
-const Menu = styled.ul`
-	border: 1px solid ${colors.gray};
-	border-radius: 3px;
-	display: inline-block;
-	font-family: 'Roboto', 'Arial', sans-serif;
-	margin: 0;
-	padding: 0;
-`;
-
-const MenuItem = styled.li`
-	cursor: pointer;
-	display: inline-block;
-	padding: .5em 1em;
-	&:hover {
-		background-color: ${colors.lightgray};
-	}
-	&:not(:first-child) {
-		border-left: 1px solid ${colors.gray};
-	}
-	&.active {
-		background-color: steelblue;
-		color: #fff;
-	}
-`;
 
 const DropdownContainer = styled.div`
 	border: 1px solid ${colors.gray};
@@ -57,7 +37,7 @@ const DropdownContainer = styled.div`
 		border-color: ${colors.lightgray};
 		border-style: solid;
 		border-width: 1px 0 0 1px;
-		display: ${props => props.isOpen ? 'block' : 'none'};
+		display: ${props => (props.isOpen ? "block" : "none")};
 		height: 1em;
 		position: absolute;
 		bottom: calc(-1.75em);
@@ -68,11 +48,11 @@ const DropdownContainer = styled.div`
 `;
 
 const DropdownTrigger = styled.a`
-	color: ${props => props.focused ? 'red' : 'inherit'};
+	color: ${props => (props.focused ? "red" : "inherit")};
 	display: inline-block;
 	padding: .5em 1em;
 	text-decoration: none;
-`
+`;
 
 /**
  * <Menu>
@@ -82,9 +62,30 @@ const DropdownTrigger = styled.a`
 	</Menu>
  */
 
-const MenuWrapper = styled.div`
-	border: ${({ isOpen }) =>
-		isOpen ? `1px solid ${colors.lightgray}` : "none"};
+const animation_fadein = keyframes`
+	from {
+		opacity: 0
+	}
+	to {
+		opacity: 1
+	}
+`
+
+const animation_fadeout = keyframes`
+	from {
+		opacity: 1
+	}
+	to {
+		opacity: 0
+	}
+`
+
+// opacity: ${({ isOpen }) => (isOpen ? "1" : 0)};
+// transition: .2s opacity;
+
+const DropdownWrapper = styled.div`
+	animation: ${animation_fadein} .2s linear forward;
+	border: ${({ isOpen }) => (isOpen ? `1px solid ${colors.lightgray}` : "none")};
 	border-radius: 3px;
 	display: inline-block;
 	height: ${({ isOpen }) => (isOpen ? "auto" : 0)};
@@ -96,80 +97,85 @@ const MenuWrapper = styled.div`
 	min-width: calc(100% - 2em);
 `;
 
-class Expandable extends Component {
-	render() {
-		const { children, ...rest} = this.props
-		return children(...rest)
-	}
-}
-
 class Dropdown extends Component {
 	constructor() {
 		super();
 		this.state = { menuIsOpen: false, menuFocus: false, triggerFocus: false };
-		this.updateMenuState = this.updateMenuState.bind(this);
 		this.toggleMenu = this.toggleMenu.bind(this);
-		this.handleMenu = this.handleMenu.bind(this)
+		this.handleMenu = this.handleMenu.bind(this);
 	}
 	componentDidMount() {
-		window.addEventListener('click',e => {
-			if (e.target.tagName === 'HTML') {
-				this.setState(prevState => ({
-					menuIsOpen: false,
-					menuFocus: false
-				}))
-			}
-		})
+		// window.addEventListener("click", e => {
+		// 	if (e.target.tagName === "HTML") {
+		// 		this.setState(prevState => ({
+		// 			menuIsOpen: false,
+		// 			menuFocus: false
+		// 		}));
+		// 	}
+		// });
 	}
-	updateMenuState({ menuIsOpen, menuFocus = true }) {
-		this.setState(prevState => ({ menuIsOpen: prevState.triggerFocus || (menuFocus ? true : false) , menuFocus: menuFocus || (prevState.triggerFocus) }));
-	}
-	toggleMenu(callback = (() => null)) {
-		this.setState(prevState => {
-			return ({ menuIsOpen: !prevState.menuIsOpen, menuFocus: true })
-		}, callback())
+	toggleMenu() {
+		this.setState(prevState => ({ menuIsOpen: !prevState.menuIsOpen, menuFocus: true }));
 	}
 	handleMenu({ keepOpen } = { keepOpen: false }) {
 		this.setState(prevState => ({
 			menuIsOpen: keepOpen,
 			menuFocus: keepOpen
-		}))
+		}));
 	}
 	render() {
 		return (
 			<DropdownContainer isOpen={this.state.menuIsOpen && this.state.menuFocus}>
-				<DropdownTrigger href
-					onClick={e => { e.preventDefault(); this.toggleMenu()}}
-					onMouseEnter={()=> this.setState(prevState => ({ menuIsOpen: prevState.menuIsOpen && !prevState.menuFocus ? false : prevState.menuIsOpen }))}
-					onBlur={() => null /*this.setState(prevState => ({ menuFocus: false }))*/}
+				<DropdownTrigger
+					onClick={e => {
+						e.preventDefault();
+						this.toggleMenu();
+					}}
+					onMouseEnter={() =>
+						this.setState(prevState => ({
+							menuIsOpen: prevState.menuIsOpen && !prevState.menuFocus ? false : prevState.menuIsOpen
+						}))}
 				>
-				My Menu
+					My Menu
 				</DropdownTrigger>
-				<MenuWrapper 
-					data-dropdown="menuwrapper"
-					isOpen={this.state.menuIsOpen && this.state.menuFocus} 
-					onClick={() => null/*this.setState(prevState => ({ menuFocus: false }))*/}
-					onMouseEnter={()=> null/*this.setState(prevState => ({ menuFocus: true }))*/}
-					onMouseLeave={()=> this.setState(prevState => ({ menuFocus: false }))}>
-					<Expandable>
-						{(props) => this.props.children({ props, handleMenu: this.handleMenu })}
-					</Expandable>
-				</MenuWrapper>
+				<DropdownWrapper
+					data-dropdown-id="wrapper"
+					isOpen={this.state.menuIsOpen && this.state.menuFocus}
+					onMouseLeave={() => this.setState(prevState => ({ menuFocus: false }))}
+				>
+					<_RenderChildrenFromFn>
+						{props => this.props.children({ handleMenu: this.handleMenu })}
+					</_RenderChildrenFromFn>
+				</DropdownWrapper>
 			</DropdownContainer>
 		);
 	}
 }
 
-const logRoute = route => callback => { console.log(route); callback() }
+const logRoute = route => callback => {
+	console.log(route);
+	callback();
+};
 
-/**
- * onClick={e => this.setState(prevState => ({ menuFocus: true }))}
-onClick={e => this.setState(prevState => ({ menuFocus: true }))}
-onClick={e => this.setState(prevState => ({ menuFocus: true }))}
-**/
-storiesOf("Menu/Dropdown", module).add("default", () => <Dropdown>
-					{({ props, handleMenu }) => (<ul style={{ padding: 0, listStyleType: "none" }} {...props}>
-						<li><a href="#Home" onClick={() => logRoute('/home')(() => handleMenu({keepOpen: true}))}>Home</a></li>
-						<li><a href="#About" onClick={() => logRoute('/about')(handleMenu)}>About</a></li>
-						<li><a href="#Contact" onClick={() => logRoute('/contact')(handleMenu)}>Contact</a></li>
-					</ul>)}</Dropdown>);
+storiesOf("Menu/Dropdown", module).add("default", () =>
+	<Dropdown>
+		{({ handleMenu }) =>
+			<ul style={{ padding: 0, listStyleType: "none" }} >
+				<li>
+					<a href="#Home" onClick={() => logRoute("/home")(() => handleMenu({ keepOpen: true }))}>
+						Home
+					</a>
+				</li>
+				<li>
+					<a href="#About" onClick={() => logRoute("/about")(handleMenu)}>
+						About
+					</a>
+				</li>
+				<li>
+					<a href="#Contact" onClick={() => logRoute("/contact")(handleMenu)}>
+						Contact
+					</a>
+				</li>
+			</ul>}
+	</Dropdown>
+);
